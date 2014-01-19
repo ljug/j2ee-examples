3d0ddf0088e1b7b817b4076f93385adeca3d9e02
pascalfares <pascal.fares@cofares.net>
2014-01-19 02:28:45 +0200

    Preparstion datatable primeface avec maven

---
+++ JSFEx/datatablePrime/src/main/java/net/cofares/datatableprime/OrderBean.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.cofares.datatableprime;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author pfares
+ */
+@ManagedBean
+@SessionScoped
+public class OrderBean {
+
+    /**
+     * Creates a new instance of Order
+     */
+    public OrderBean() {
+    }
+    
+}
